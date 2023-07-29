1cb59f2da982a23507a2cf22eac7509aaea982d3
DevilofParadis <114922080+DevilofParadis@users.noreply.github.com>
2023-07-29 17:15:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001920102302"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5751548638"))
 
 #Port
-PORT = os.environ.get("PORT", "8091")
+PORT = os.environ.get("PORT", "8099")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kacxx1:mongo123@cluster0.ownatea.mongodb.net/?retryWrites=true&w=majority")
